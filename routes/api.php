069e553ce77ec708522cf24fa0a58d0bbfa73f89
Felipe Farias <felipe.farias.php@gmail.com>
2021-03-01 12:27:36 -0300

    configurado acesso api https://github.com/fruitcake/laravel-cors

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//http://127.0.0.1:8000/api/teste
+Route::get('/teste', function () {
+    return ('teste api');
+});
