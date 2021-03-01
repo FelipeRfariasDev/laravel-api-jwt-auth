8a9d1f98657d734aa8eabb751e1859f2991bb74c
Felipe Farias <felipe.farias.php@gmail.com>
2021-03-01 12:31:56 -0300

    nome e email para contatos

--- database/migrations/2021_03_01_122822_create_contatos_table.php
+++ database/migrations/2021_03_01_122822_create_contatos_table.php
@@ -15,6 +15,8 @@ class CreateContatosTable extends Migration
     {
         Schema::create('contatos', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
             $table->timestamps();
         });
     }
