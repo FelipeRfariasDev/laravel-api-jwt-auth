27e97fd66b7553a032e0aaf36560cfb4c663d5f3
Felipe Farias <felipe.farias.php@gmail.com>
2021-03-03 16:50:13 -0300

    update ApiRotasProtegidas

--- app/Http/Middleware/ApiRotasProtegidas.php
+++ app/Http/Middleware/ApiRotasProtegidas.php
@@ -5,11 +5,10 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Tymon\JWTAuth\Exceptions\TokenInvalidException;
-use \Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 
-class ApiRotasProtegidas extends VerifyCsrfToken
+class ApiRotasProtegidas
 {
     /**
      * Handle an incoming request.
