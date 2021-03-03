e2a76776c0003969cb88a8926b368d607c9e1c8d
Felipe Farias <felipe.farias.php@gmail.com>
2021-03-03 10:22:23 -0300

    rotas api contatos

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,18 @@ use App\Http\Controllers\ContatosController;
 |
 */
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
 
 //http://127.0.0.1:8000/api/contatos
-Route::resource('contatos', ContatosController::class);
+//Route::resource('contatos', ContatosController::class);
+
+//http://127.0.0.1:8000/api/contatos
+Route::get('contatos', 'App\Http\Controllers\ContatosController@index');
+Route::get('contatos/{id}', 'App\Http\Controllers\ContatosController@show');
+Route::post('contatos', 'App\Http\Controllers\ContatosController@store');
+Route::put('contatos/{id}', 'App\Http\Controllers\ContatosController@update');
+Route::delete('contatos/{id}', 'App\Http\Controllers\ContatosController@destroy');
