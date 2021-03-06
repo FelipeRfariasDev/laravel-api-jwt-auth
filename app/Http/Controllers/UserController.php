45323b5251e4cdd8ef1f48fa44fb4bfe9e544c4a
Felipe Farias <felipe.farias.php@gmail.com>
2021-03-06 16:07:10 -0300

    login

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    private $model;
+
+    public function __construct(User $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Salvar método HTTP POST
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->model->fill([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        return response()->json($this->model->save(), 201);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        if (!$token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -24,19 +24,36 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //Route::resource('contatos', ContatoController::class);
 
 //http://127.0.0.1:8000/api/contatos
+
+/*
+Route::post('login', 'App\Http\Controllers\Api\AuthController@login');
+
+Route::post('logout', 'App\Http\Controllers\Api\AuthController@logout');
+Route::post('me', 'App\Http\Controllers\Api\AuthController@me');
+Route::post('refresh', 'App\Http\Controllers\Api\AuthController@refresh');
+*/
+
+Route::post('login', 'App\Http\Controllers\UserController@login');
+Route::post('usuarios', 'App\Http\Controllers\UserController@store');
+
 Route::get('contatos', 'App\Http\Controllers\ContatoController@index');
 Route::get('contatos/{id}', 'App\Http\Controllers\ContatoController@show');
 Route::post('contatos', 'App\Http\Controllers\ContatoController@store');
 Route::put('contatos/{id}', 'App\Http\Controllers\ContatoController@update');
 Route::delete('contatos/{id}', 'App\Http\Controllers\ContatoController@destroy');
 
-Route::post('login', 'App\Http\Controllers\Api\AuthController@login');
-
 Route::group(['middleware' => 'apiJwt'], function () {
-
+    /*
     Route::post('logout', 'App\Http\Controllers\Api\AuthController@logout');
     Route::post('me', 'App\Http\Controllers\Api\AuthController@me');
     Route::post('refresh', 'App\Http\Controllers\Api\AuthController@refresh');
 
     Route::get('users', 'App\Http\Controllers\Api\UserController@index');
+
+    Route::get('contatos', 'App\Http\Controllers\ContatoController@index');
+    Route::get('contatos/{id}', 'App\Http\Controllers\ContatoController@show');
+    Route::post('contatos', 'App\Http\Controllers\ContatoController@store');
+    Route::put('contatos/{id}', 'App\Http\Controllers\ContatoController@update');
+    Route::delete('contatos/{id}', 'App\Http\Controllers\ContatoController@destroy');
+    */
 });
